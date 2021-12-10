65bbdf0ce2c7c206adb635c998d3d9048493fa84
agrawalyuvraj <yuvrajagrawal11@gmail.com>
2021-12-10 13:30:07 +0530

    Users database table modified
    Added columns for username, otp and otpGenerationTime

--- src/main/java/com/electioncomission/ec/entity/Users.java
+++ src/main/java/com/electioncomission/ec/entity/Users.java
@@ -11,6 +11,7 @@ import lombok.ToString;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.sql.Timestamp;
 
 @Entity
 @Table(name = "users")
@@ -29,20 +30,24 @@ public class Users {
     @Size(max = 30, message = "size cannot be greater than ")
     String lastName;
 
-    @NotBlank(message = "Mobile Number cannot be blank")
     @Size(min = 10, max = 10, message = "size cannot be greater than ")
     String mobileNumber;
+
+    @Size(min=6, max=6, message = "size cannot be greater than")
+    String otp;
+
+    Timestamp otpGenerationTime;
+
+    @Size(max = 30, message = "size cannot be greater than ")
+    String userName;
+
+    String password;
 
     @NotBlank(message = "User Role cannot be null")
     @Size(max = 5, message = "size cannot be greater than ")
     String userRole;
 
-
-    String password;
-
     Integer partId;
     Integer constituencyId;
     Integer districtId;
-
-
 }
